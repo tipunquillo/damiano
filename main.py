6c6ded93307fc0fec48d292f92fb3581d095c7d0
Alejandro <68978152+tipunquillo@users.noreply.github.com>
2024-01-30 08:18:32 -0600

    Agregue el main
    Tiene lo necesario para que se inicie el bot

---
+++ main.py
@@ -0,0 +1,11 @@
+import discord
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+	print("El bot esta encendido")
+
+client.run('')
