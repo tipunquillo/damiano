f41916e31ac704c691f9f51b64592a99319909f8
Alejandro <68978152+tipunquillo@users.noreply.github.com>
2024-01-31 12:54:13 -0600

    Actualice el puerto

--- app.py
+++ app.py
@@ -1,21 +1,9 @@
-# app.py
-
 from flask import Flask
-import os
-
 app = Flask(__name__)
 
-@app.route('/healthz') 
+@app.route('/healthz')
 def healthCheck():
-  print("/healthz endpoint llamado")
-  return "OK"
+    return "OK" 
 
 if __name__ == "__main__":
-
-    print("Iniciando servidor Flask...")
-    
-    PORT = os.getenv('PORT') if os.getenv('PORT') else 8080 
-    
-    app.run(host='0.0.0.0', port=PORT)
-    
-    print("Servidor Flask inicializado en puerto", PORT)
+   app.run(port='3000')
