1c810cb1f1621e110cec172fc0dd305217861d9e
Alejandro <68978152+tipunquillo@users.noreply.github.com>
2024-01-31 12:23:35 -0600

    Probando otro puerto

--- app.py
+++ app.py
@@ -5,15 +5,17 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/healthz')
+@app.route('/healthz') 
 def healthCheck():
-  print("/healthz llamado")
+  print("/healthz endpoint llamado")
   return "OK"
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
+
     print("Iniciando servidor Flask...")
     
-    PORT = os.getenv('PORT') if os.getenv('PORT') else 3000
+    PORT = os.getenv('PORT') if os.getenv('PORT') else 8080 
     
     app.run(host='0.0.0.0', port=PORT)
+    
     print("Servidor Flask inicializado en puerto", PORT)
