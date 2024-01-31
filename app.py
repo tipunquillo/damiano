7d4d372174555e6f10dadb7728273bd5a9ecc970
Alejandro <68978152+tipunquillo@users.noreply.github.com>
2024-01-31 13:12:07 -0600

    Regrese al puerto predeterminado

--- app.py
+++ app.py
@@ -1,19 +1,10 @@
-# app.py
-
 from flask import Flask
-import os
-
 app = Flask(__name__)
 
 @app.route('/healthz')
 def healthCheck():
-  print("/healthz llamado")
-  return "OK"
+    return "OK" 
 
-if __name__ == "__main__": 
-    print("Iniciando servidor Flask...")
-    
-    PORT = os.getenv('PORT') if os.getenv('PORT') else 3000
-    
-    app.run(host='0.0.0.0', port=PORT)
-    print("Servidor Flask inicializado en puerto", PORT)
+if __name__ == "__main__":
+   app.run(port = 3000)
+  #app.run(PORT = 3000)
