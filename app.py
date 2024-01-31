08297127f883160b788e3ba2fed19460d639215d
Alejandro <68978152+tipunquillo@users.noreply.github.com>
2024-01-31 13:04:40 -0600

    Cambie el puerto predefinido

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
+# app.py
+
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
-@app.route('/healthz') 
+@app.route('/healthz')
 def healthCheck():
-    return "OK"
+  print("/healthz llamado")
+  return "OK"
 
-if __name__ == "__main__":
-   app.run(host ='0.0.0.0', PORT = 3000)
+if __name__ == "__main__": 
+    print("Iniciando servidor Flask...")
+    
+    PORT = os.getenv('PORT') if os.getenv('PORT') else 3000
+    
+    app.run(host='0.0.0.0', port=PORT)
+    print("Servidor Flask inicializado en puerto", PORT)
